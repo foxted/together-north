d30e200228a72c68e985586ccdb4facdf1495c25
Valentin Prugnaud <valentin@whatdafox.com>
2020-03-20 07:14:55 -0700

    chore: add feedback link

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="bg-white">
+    <div class="bg-white pb-6">
         <div class="max-w-screen-xl mx-auto pt-6 px-4 overflow-hidden sm:px-6 lg:px-8">
-            <p class="text-center">
+            <p class="text-center text-sm mb-4">
+                <a href="https://docs.google.com/forms/d/1A5fkkR98bb0xECzcNWatRHISSPIiZgV6R7oCAD91U1Y/edit" target="_blank" class="text-gray-400 hover:text-gray-500">
+                    Give us feedback
+                </a>
                 <a href="https://github.com/foxted/together-north" target="_blank" class="ml-6 text-gray-400 hover:text-gray-500">
                     <i class="fab fa-github"></i>&nbsp;Contribute on GitHub
                 </a>
